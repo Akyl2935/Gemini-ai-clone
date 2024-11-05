d8fe09632cbfb7ac73d112b1751d8c088cf05694
akyl2935 <jpak2935@gmail.com>
2024-11-05 20:25:58 +0100

    added assets line

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Sidebar.css'
+import {assets} from '../../assets/assets'
+
+const sidebar = () => {
+  return (
+    <div className ='sidebar'>
+    <div className = "top">
+    <img src = "" alt = "" />
+    </div>
+    <div className = "bottom"></div>
+    </div>
+  )
+}
+
+export default sidebar
