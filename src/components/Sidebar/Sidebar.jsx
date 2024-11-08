611232147475db7aa0a439753b5a5524d0cb5a2f
akyl2935 <jpak2935@gmail.com>
2024-11-08 22:33:44 +0100

    adding img src

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -9,6 +9,11 @@ const sidebar = () => {
     <img className = 'menu' src = {assets.menu_icon} alt = "" />
     <div className = "new-chat">
       <img src = {assets.plus_icon} alt = ""/>
+      <p>New Chat</p>
+    </div>
+    <div className = "recent">
+      <p className = "recent-title">Recent</p>
+      <div className = "recent-entry"><img src = {assets.message_icon}/></div>
     </div>
     </div>
     <div className = "bottom"></div>
