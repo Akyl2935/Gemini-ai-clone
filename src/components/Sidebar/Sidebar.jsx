b3c92530571cbf7ba892b3c5d5b048bbe58c8a90
akyl2935 <jpak2935@gmail.com>
2024-11-22 18:57:30 +0100

    fix Sidebar name

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import './Sidebar.css'
 import {assets} from '../../assets/assets'
 
-const sidebar = () => {
+const Sidebar = () => {
   return (
     <div className ='sidebar'>
     <div className = "top">
-    <img className = 'menu' src = {assets.menu_icon} alt = "" />
+    <img className = 'menu' src={assets.menu_icon} alt = "" />
     <div className = "new-chat">
       <img src = {assets.plus_icon} alt = ""/>
       <p>New Chat</p>
